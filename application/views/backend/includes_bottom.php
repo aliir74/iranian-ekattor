06bbcfebf1f23457bd802e78e043b9f84482d624
aliir74 <aliirani74@gmail.com>
2017-10-20 15:54:47 +0330

    add fullcalendar source to include

--- application/views/backend/includes_bottom.php
+++ application/views/backend/includes_bottom.php
@@ -18,6 +18,11 @@
 	<script src="assets/js/toastr.js"></script>
     <script src="assets/js/jquery.validate.min.js"></script>
 	<script src="assets/js/fullcalendar/fullcalendar.min.js"></script>
+    <!--manually added for jalaali calendar-->
+    <script src="assets/js/fullcalendar/lang/fa.js"></script>
+    <script src="assets/js/fullcalendar/lib/moment.js"></script>
+    <script src="assets/js/fullcalendar/lib/moment-jalaali.js"></script>
+    <!---->
     <script src="assets/js/bootstrap-datepicker.js"></script>
     <script src="assets/js/fileinput.js"></script>
     
