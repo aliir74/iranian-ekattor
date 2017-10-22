e11bb572a9abf38540e13cc056e64876bb5a282b
aliir74 <aliirani74@gmail.com>
2017-10-22 22:14:37 +0330

    change school tiltle in panel header ( hard code it )

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -1,6 +1,7 @@
 <div class="row">
 	<div class="col-md-12 col-sm-12 clearfix" style="text-align:center;">
-		<h2 style="font-weight:200; margin:0px;"><?php echo $system_name;?></h2>
+		<!--<h2 style="font-weight:200; margin:0px;"><?php /*echo $system_name;*/?></h2>-->
+        <h2 style="font-weight:200; margin:0px;">دبیرستان دوره اول علامه حلی ۲ تهران</h2>
     </div>
 	<!-- Raw Links -->
 	<div class="col-md-12 col-sm-12 clearfix ">
