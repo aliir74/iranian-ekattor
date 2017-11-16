a01d1c910fde13fbcd175bd8d68baf6520784c8e
aliir74 <aliirani74@gmail.com>
2017-11-16 15:06:40 +0330

    some minor pedram changes :D

--- application/models/Email_model.php
+++ application/models/Email_model.php
@@ -2,45 +2,46 @@
 
 class Email_model extends CI_Model {
 
-	function __construct()
+    function __construct()
     {
         parent::__construct();
     }
 
-	function account_opening_email($account_type = '' , $email = '', $password = '')
-	{
-		$system_name	=	$this->db->get_where('settings' , array('type' => 'system_name'))->row()->description;
+    function account_opening_email($account_type = '' , $email = '', $password = '')
+    {
+        $system_name	=	$this->db->get_where('settings' , array('type' => 'system_name'))->row()->description;
 
-		$email_msg		=	"Welcome to ".$system_name."<br />";
-		$email_msg		.=	"Your account type : ".$account_type."<br />";
-		$email_msg		.=	"Your login password : ". $password ."<br />";
-		$email_msg		.=	"Login Here : ".base_url()."<br />";
+        $email_msg		=	"خوش آمدید ".$system_name." به<br />";
+        $email_msg		.=	"نوع حساب کاربری : ".$account_type."<br />";
+        $email_msg		.=	"نام کاربری : ".$email."<br />";
+        //	$email_msg		.=	"رمز عبور : ". $password ."<br />";
+        $email_msg		.=	"لینک ورود : ".base_url()."<br />";
 
-		$email_sub		=	"Account opening email";
-		$email_to		=	$email;
+        $email_sub		=	"مشخصات حساب کاربری";
+        $email_to		=	$email;
 
-		$this->do_email($email_msg , $email_sub , $email_to);
-	}
+        $this->do_email($email_msg , $email_sub , $email_to);
+    }
 
-	function password_reset_email($new_password = '' , $account_type = '' , $email = '')
-	{
-		$query			=	$this->db->get_where($account_type , array('email' => $email));
-		if($query->num_rows() > 0)
-		{
+    function password_reset_email($new_password = '' , $account_type = '' , $email = '')
+    {
+        $query			=	$this->db->get_where($account_type , array('email' => $email));
+        if($query->num_rows() > 0)
+        {
 
-			$email_msg	=	"Your account type is : ".$account_type."<br />";
-			$email_msg	.=	"Your password is : ".$new_password."<br />";
+            $email_msg	=	"نوع حساب کاربری : ".$account_type."<br />";
+            $email_msg	.=	"رمز عبور جدید شما : ".$new_password."<br />";
 
-			$email_sub	=	"Password reset request";
-			$email_to	=	$email;
-			$this->do_email($email_msg , $email_sub , $email_to);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+            $email_sub	=	"درخواست بازنشانی رمز عبور";
+            $email_to	=	$email;
+            $this->do_email($email_msg , $email_sub , $email_to);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     function notify_email($email_sub, $email_message) {
         //$this->contact_message_email($email_message['sender'],$email_message=['reciever'], $email_message);
@@ -65,16 +66,16 @@ class Email_model extends CI_Model {
         $this->do_email($message, $email_sub, $email_to, 'admin@helli2portal.ir');
     }
 
-	function contact_message_email($email_from, $email_to, $email_message) {
-		$email_sub = "Message from School Website";
-		$this->do_email($email_message, $email_sub, $email_to, $email_from);
-	}
+    function contact_message_email($email_from, $email_to, $email_message) {
+        $email_sub = "پیام از سسیستم مدیریت مدرسه";
+        $this->do_email($email_message, $email_sub, $email_to, $email_from);
+    }
 
-	/***custom email sender****/
-	function do_email($msg=NULL, $sub=NULL, $to=NULL, $from=NULL)
-	{
+    /***custom email sender****/
+    function do_email($msg=NULL, $sub=NULL, $to=NULL, $from=NULL)
+    {
 
-		$config = array();
+        $config = array();
         $config['useragent']	= "CodeIgniter";
         $config['mailpath']		= "/usr/bin/sendmail"; // or "/usr/sbin/sendmail"
         $config['protocol']		= "smtp";
@@ -89,20 +90,21 @@ class Email_model extends CI_Model {
 
         $this->email->initialize($config);
 
-		$system_name	=	$this->db->get_where('settings' , array('type' => 'system_name'))->row()->description;
-		if($from == NULL)
-			$from		=	$this->db->get_where('settings' , array('type' => 'system_email'))->row()->description;
+        $system_name	=	$this->db->get_where('settings' , array('type' => 'system_name'))->row()->description;
+        if($from == NULL)
+            $from		=	$this->db->get_where('settings' , array('type' => 'system_email'))->row()->description;
 
-		$this->email->from($from, $system_name);
-		$this->email->from($from, $system_name);
-		$this->email->to($to);
-		$this->email->subject($sub);
+        $this->email->from($from, $system_name);
+        $this->email->from($from, $system_name);
+        $this->email->to($to);
+        $this->email->subject($sub);
 
-		$msg	=	$msg."<br /><br /><br /><br /><br /><br /><br /><hr /><center><a href=\"http://codecanyon.net/item/ekattor-school-management-system-pro/6087521?ref=joyontaroy\">&copy; 2013 Ekattor School Management System Pro</a></center>";
-		$this->email->message($msg);
+        $msg	=	$msg."<br /><br /><br /><br /><br /><br /><br /><hr /><center><a href=\"http://helli2portal.ir\">دبیرستان دوره اول علامه حلی ۲ تهران</a></center>";
 
-		$this->email->send();
+        $this->email->message($msg);
 
-		//echo $this->email->print_debugger();
-	}
+        $this->email->send();
+
+        //echo $this->email->print_debugger();
+    }
 }
--- application/views/backend/footer.php
+++ application/views/backend/footer.php
@@ -1,7 +1,5 @@
 <!-- Footer -->
 <footer class="main">
-	&copy; 2017 <strong>Ekattor School Manager | Version 5.0</strong>.
-    Developed by
-	<a href="http://creativeitem.com"
-    	target="_blank">Creativeitem</a>
+    <strong>دبیرستان دوره اول علامه حلی ۲ تهران | </strong>
+    سیستم مدیریت مدرسه
 </footer>
