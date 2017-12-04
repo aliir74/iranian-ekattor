5067d55bb57fd2de45794608d8e8268033b7cad6
aliir74 <aliirani74@gmail.com>
2017-12-04 23:25:37 +0330

    add test code for kavenegar sms api

--- application/views/backend/login.php
+++ application/views/backend/login.php
@@ -7,8 +7,8 @@ $system_name = "دبیرستان دوره اول علامه حلی ۲ تهران
 
 <?php
 $this->sms_model->send_sms($message,$receiver_phone);
-toastr.success('hello');
 ?>
+
 <html class="no-js" lang="">
     <head>
       <meta charset="utf-8">
@@ -79,5 +79,9 @@ toastr.success('hello');
       </script>
     <?php } ?>
 
+
+        <script type="text/javascript">
+         toastr.success('hello');
+        </script>
     </body>
 </html>
