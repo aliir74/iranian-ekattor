056a7f105ab9c97c9fba414a8a73f4da2d5204e0
aliir74 <aliirani74@gmail.com>
2017-12-05 16:10:09 +0330

    some show_error function

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -31,7 +31,8 @@ class Sms_model extends CI_Model {
 
     // SEND SMS VIA KAVENEGAR API
     function send_sms_via_kavenegar($message = '', $reciever_phone = '') {
-        log_message('error', 'into sms function');
+        #log_message('error', 'into sms function');
+        show_error('../../vendor/autoload.php');
         require '../../vendor/autoload.php';
         show_error('into sms function');
         try{
