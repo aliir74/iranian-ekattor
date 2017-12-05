4f551a4c6f35714fb7a398a85f7df66227d6526e
aliir74 <aliirani74@gmail.com>
2017-12-05 13:00:01 +0330

    remove before require show_error

--- application/models/Sms_model.php
+++ application/models/Sms_model.php
@@ -32,7 +32,6 @@ class Sms_model extends CI_Model {
     // SEND SMS VIA KAVENEGAR API
     function send_sms_via_kavenegar($message = '', $reciever_phone = '') {
         #log_message('error', 'into sms function');
-        show_error('before require');
         require '../../vendor/autoload.php';
         show_error('into sms function');
         try{
