93e29cff547e208ae672c6116cf129e1179cbe2b
aliir74 <aliirani74@gmail.com>
2017-12-06 15:09:08 +0330

    remove show_error

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -241,7 +241,7 @@ class Crud_model extends CI_Model {
         global $jdate;
         #$data['timestamp']         = strtotime($this->input->post('timestamp'));
         $var = explode('/', $this->input->post('timestamp'));
-        show_error($var);
+        echo ($var);
         $data['timestamp']         = $jdate->mktime(0, 0, 0, (int)$var[0], (int)$var[1], (int)$var[2]);
         $data['title'] 		       = $this->input->post('title');
         $data['description']       = $this->input->post('description');
