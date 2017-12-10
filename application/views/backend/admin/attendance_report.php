1e4d6e1b8801cfbca12f5e24c2e0a2bd61446927
aliir74 <aliirani74@gmail.com>
2017-12-10 23:23:44 +0330

    test for getting number of persian month of now

--- application/views/backend/admin/attendance_report.php
+++ application/views/backend/admin/attendance_report.php
@@ -15,7 +15,7 @@ $jdate = new jDateTime(true, true, 'Asia/Tehran');
         ?>
 
         <div class="col-md-4">
-            <?php echo $jdate->date('m', false); ?>
+            <?php echo $jdate->date('m', false, false); ?>
             <div class="form-group">
                 <label class="control-label" style="margin-bottom: 5px;"><?php echo get_phrase('class'); ?></label>
                 <select class="form-control selectboxit" name="class_id" onchange="select_section(this.value)">
