ecd7f5448086cf2356a7e233b581cad94ad63a98
aliir74 <aliirani74@gmail.com>
2018-01-18 17:36:11 +0330

    change view

--- application/views/backend/admin/class.php
+++ application/views/backend/admin/class.php
@@ -126,7 +126,7 @@
 </div>
 <div class="row">
     <div class="col-md-3"></div>
-    <div class="col-md-6" style="padding: 15px;">
+    <div class="col-md-6" style="padding: 15px; text-align: center">
         <button type="button" class="btn btn-primary" name="generate_csv" id="generate_csv"><?php echo "گرفتن اطلاعات دانش آموزان"; ?></button>
     </div>
     <div class="col-md-3"></div>
